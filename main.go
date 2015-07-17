cdc398e75fd9876add6e790f3a18dd513fb1f4a0
David Oram <david.oram99@gmail.com>
2015-07-18 08:10:22 +1200

    WIP

--- main.go
+++ main.go
@@ -1,17 +1,109 @@
 package main
 
-import "github.com/3d0c/gmf"
+import (
+	"log"
+	"os"
+
+	"github.com/davidoram/gmf"
+)
+
+func fatal(e error) {
+	log.Fatal(e)
+}
+
+func abort(s string) {
+	log.Fatal(s)
+}
+
+type FilterContext struct {
+	src  *gmf.AVFilter
+	sink *gmf.AVFilter
+}
+
+func initFilters() FilterContext {
+	src, err := gmf.GetFilter("buffer")
+	if err != nil {
+		fatal(err)
+	}
+	sink, err := gmf.GetFilter("buffersink")
+	if err != nil {
+		fatal(err)
+	}
+	return FilterContext{src: src, sink: sink}
+}
 
 func main() {
 
-	url := "rtsp://81.25.188.121:554/axis-media/media.amp?videocodec=h264&compression=30&fps=10&resolution=4CIF&videokeyframeinterval=30&camera=1"
-
-	ctx, err := gmf.NewInputCtx(url)
-	if err != nil {
-		panic(err)
+	if len(os.Args) != 2 {
+		log.Fatal("Usage: %s file\n", os.Args[0])
 	}
-	println("ok")
-	defer ctx.CloseInputAndRelease()
-	//...
 
+	ictx := gmf.NewCtx()
+	log.Printf("Opening input '%v'...\n", os.Args[1])
+	ictx.OpenInput(os.Args[1])
+	log.Println("Retrieving best stream...")
+	ist, err := ictx.GetBestStream(gmf.AVMEDIA_TYPE_VIDEO)
+	if err != nil {
+		fatal(err)
+	}
+
+	initFilters()
+
+	log.Println("Reading packets...")
+	var i = 0
+	for p := range ictx.GetNewPackets() {
+		_ = p
+		i++
+		gmf.Release(p)
+	}
+	log.Println("Done", i, ist)
+
+	ictx.CloseInputAndRelease()
+
+	//   if ((ret = init_filters(filter_descr)) < 0)
+	//       goto end;
+	//   /* read all packets */
+	//   while (1) {
+	//       if ((ret = av_read_frame(fmt_ctx, &packet)) < 0)
+	//           break;
+	//       if (packet.stream_index == video_stream_index) {
+	//           got_frame = 0;
+	//           ret = avcodec_decode_video2(dec_ctx, frame, &got_frame, &packet);
+	//           if (ret < 0) {
+	//               av_log(NULL, AV_LOG_ERROR, "Error decoding video\n");
+	//               break;
+	//           }
+	//           if (got_frame) {
+	//               frame->pts = av_frame_get_best_effort_timestamp(frame);
+	//               /* push the decoded frame into the filtergraph */
+	//               if (av_buffersrc_add_frame_flags(buffersrc_ctx, frame, AV_BUFFERSRC_FLAG_KEEP_REF) < 0) {
+	//                   av_log(NULL, AV_LOG_ERROR, "Error while feeding the filtergraph\n");
+	//                   break;
+	//               }
+	//               /* pull filtered frames from the filtergraph */
+	//               while (1) {
+	//                   ret = av_buffersink_get_frame(buffersink_ctx, filt_frame);
+	//                   if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF)
+	//                       break;
+	//                   if (ret < 0)
+	//                       goto end;
+	//                   display_frame(filt_frame, buffersink_ctx->inputs[0]->time_base);
+	//                   av_frame_unref(filt_frame);
+	//               }
+	//               av_frame_unref(frame);
+	//           }
+	//       }
+	//       av_free_packet(&packet);
+	//   }
+	// end:
+	//   avfilter_graph_free(&filter_graph);
+	//   avcodec_close(dec_ctx);
+	//   avformat_close_input(&fmt_ctx);
+	//   av_frame_free(&frame);
+	//   av_frame_free(&filt_frame);
+	//   if (ret < 0 && ret != AVERROR_EOF) {
+	//       fprintf(stderr, "Error occurred: %s\n", av_err2str(ret));
+	//       exit(1);
+	//   }
+	//   exit(0);
 }
